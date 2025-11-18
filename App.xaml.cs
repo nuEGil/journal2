5a6a376ed1a79577c60affeaf470987c3cf715bd
nuEGil <42759839+nuEGil@users.noreply.github.com>
2025-11-17 19:57:32 -0600

    search is working now

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,19 @@
-﻿namespace journal2;
-
-public partial class App : Application
+﻿using journal2.Services;
+namespace journal2
 {
-    public App()
-	{
-        InitializeComponent();
-    }
 
-    protected override Window CreateWindow(IActivationState? activationState)
+    public partial class App : Application
     {
-        return new Window(new AppShell());
+        public App(IFileSeeder fileSeeder, IDataBaseInitializer dbi)
+        {
+            InitializeComponent();
+            Task.Run(async () => await dbi.DBInitAsync());
+            Task.Run(async () => await fileSeeder.SeedAsync());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new AppShell());
+        }
     }
 }
--- MauiProgram.cs
+++ MauiProgram.cs
@@ -2,57 +2,46 @@
 using journal2.Services;
 using journal2.ViewModels;
 
-namespace journal2;
-
-public static class MauiProgram
+namespace journal2
 {
-    public static MauiApp CreateMauiApp()
+
+    public static class MauiProgram
     {
-        var builder = MauiApp.CreateBuilder();
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
 
-        builder
-            .UseMauiApp<App>()
-            .ConfigureFonts(fonts =>
-            {
-                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
-            });
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
 
-        // ------------------------------
-        // Services
-        // ------------------------------
-        builder.Services.AddSingleton<IDataBaseInitializer, DataBaseInitializer>();
-        builder.Services.AddSingleton<IFileExplorer, FileExplorer>();
-        builder.Services.AddSingleton<IKeywordService, KeywordService>();
-        builder.Services.AddSingleton<IDatabaseService, DatabaseService>();
-        builder.Services.AddSingleton<IFileSeeder, FileSeeder>();
+            // ------------------------------
+            // Services
+            // ------------------------------
+            builder.Services.AddSingleton<IDataBaseInitializer, DataBaseInitializer>();
+            builder.Services.AddSingleton<IFileExplorer, FileExplorer>();
+            builder.Services.AddSingleton<IKeywordService, KeywordService>();
+            builder.Services.AddSingleton<IDatabaseService, DatabaseService>();
+            builder.Services.AddSingleton<IFileSeeder, FileSeeder>();
 
-        // ------------------------------
-        // ViewModels
-        // ------------------------------
-        builder.Services.AddSingleton<MainPageViewModel>();
+            // ------------------------------
+            // ViewModels
+            // ------------------------------
+            builder.Services.AddSingleton<MainPageViewModel>();
 
-        // ------------------------------
-        // Views / Pages
-        // ------------------------------
-        builder.Services.AddSingleton<MainPage>();
+            // ------------------------------
+            // Views / Pages
+            // ------------------------------
+            builder.Services.AddSingleton<MainPage>();
 
-#if DEBUG
-        builder.Logging.AddDebug();
-#endif
-
-        var app = builder.Build();
-
-        // ------------------------------
-        // Initialize Database on Startup
-        // ------------------------------
-        var db = app.Services.GetRequiredService<IDataBaseInitializer>();
-        db.DBInitAsync().Wait();   // safe at startup
-
-        var db2 = app.Services.GetRequiredService<IFileSeeder>();
-        db2.SeedAsync().Wait();   // safe at startup
-
-
-        return app;
+    #if DEBUG
+            builder.Logging.AddDebug();
+    #endif
+            return builder.Build();
+        }
     }
 }
